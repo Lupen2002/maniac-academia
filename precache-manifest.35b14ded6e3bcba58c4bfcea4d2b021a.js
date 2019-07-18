b59846dcd1957506eead06744de075d3b9f5150c
Arseniy Golushkov <arsen.gl@yandex.ru>
2019-07-18 19:57:37 +0300

    Updates

---
+++ precache-manifest.35b14ded6e3bcba58c4bfcea4d2b021a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/maniac-academia/static/media/persik.4e1ec840.png"
+  },
+  {
+    "revision": "9f0495409d356b1c9244",
+    "url": "/maniac-academia/static/js/runtime~main.af80217a.js"
+  },
+  {
+    "revision": "70bfcece9d9ee3d2960c",
+    "url": "/maniac-academia/static/js/main.efd49876.chunk.js"
+  },
+  {
+    "revision": "6a3805d4922ddc0d2b8f",
+    "url": "/maniac-academia/static/js/2.0a778a57.chunk.js"
+  },
+  {
+    "revision": "70bfcece9d9ee3d2960c",
+    "url": "/maniac-academia/static/css/main.39e0c1d9.chunk.css"
+  },
+  {
+    "revision": "6a3805d4922ddc0d2b8f",
+    "url": "/maniac-academia/static/css/2.5098cceb.chunk.css"
+  },
+  {
+    "revision": "e26f5a5d3963a6f9d001b6f684d39a89",
+    "url": "/maniac-academia/index.html"
+  }
+];
